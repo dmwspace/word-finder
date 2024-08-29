95194267286e4ab03452fce334c39a26570b3711
Dean <dmwspace@gmail.com>
2024-08-29 12:44:41 -0700

    grid letters populating

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -10,8 +10,6 @@ function Container() {
 
     console.log('gridLetters: ', gridLetters)
     
-
-
     return (
         <>
             <div id="container">
@@ -20,31 +18,31 @@ function Container() {
                     <h2>Restart</h2>
                 </div>
                 <div id="letter-grid">
-                    <div className="tile" name="zero"></div>
-                    <div className="tile" name="one"></div>
-                    <div className="tile" name="two"></div>
-                    <div className="tile" name="three"></div>
-                    <div className="tile" name="four"></div>
-                    <div className="tile" name="five"></div>
-                    <div className="tile" name="six"></div>
-                    <div className="tile" name="seven"></div>
-                    <div className="tile" name="eight"></div>
-                    <div className="tile" name="nine"></div>
-                    <div className="tile" name="ten"></div>
-                    <div className="tile" name="eleven"></div>
-                    <div className="tile" name="twelve"></div>
-                    <div className="tile" name="thirteen"></div>
-                    <div className="tile" name="fourteen"></div>
-                    <div className="tile" name="fifteen"></div>
-                    <div className="tile" name="sixteen"></div>
-                    <div className="tile" name="seventeen"></div>
-                    <div className="tile" name="eighteen"></div>
-                    <div className="tile" name="nineteen"></div>
-                    <div className="tile" name="twenty"></div>
-                    <div className="tile" name="twentyone"></div>
-                    <div className="tile" name="twentytwo"></div>
-                    <div className="tile" name="twentythree"></div>
-                    <div className="tile" name="twentyfour"></div>
+                    <div className="tile" name="zero">{gridLetters[0]}</div>
+                    <div className="tile" name="one">{gridLetters[1]}</div>
+                    <div className="tile" name="two">{gridLetters[2]}</div>
+                    <div className="tile" name="three">{gridLetters[3]}</div>
+                    <div className="tile" name="four">{gridLetters[4]}</div>
+                    <div className="tile" name="five">{gridLetters[5]}</div>
+                    <div className="tile" name="six">{gridLetters[6]}</div>
+                    <div className="tile" name="seven">{gridLetters[7]}</div>
+                    <div className="tile" name="eight">{gridLetters[8]}</div>
+                    <div className="tile" name="nine">{gridLetters[9]}</div>
+                    <div className="tile" name="ten">{gridLetters[10]}</div>
+                    <div className="tile" name="eleven">{gridLetters[11]}</div>
+                    <div className="tile" name="twelve">{gridLetters[12]}</div>
+                    <div className="tile" name="thirteen">{gridLetters[13]}</div>
+                    <div className="tile" name="fourteen">{gridLetters[14]}</div>
+                    <div className="tile" name="fifteen">{gridLetters[15]}</div>
+                    <div className="tile" name="sixteen">{gridLetters[16]}</div>
+                    <div className="tile" name="seventeen">{gridLetters[17]}</div>
+                    <div className="tile" name="eighteen">{gridLetters[18]}</div>
+                    <div className="tile" name="nineteen">{gridLetters[19]}</div>
+                    <div className="tile" name="twenty">{gridLetters[20]}</div>
+                    <div className="tile" name="twentyone">{gridLetters[21]}</div>
+                    <div className="tile" name="twentytwo">{gridLetters[22]}</div>
+                    <div className="tile" name="twentythree">{gridLetters[23]}</div>
+                    <div className="tile" name="twentyfour">{gridLetters[24]}</div>
                 </div>
                 <div id="timer"><h2>Timer</h2></div>
                 <div id="answers"><h2>Answers</h2></div>
